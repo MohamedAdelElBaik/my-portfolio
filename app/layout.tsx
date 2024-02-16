4f6b6235fbf63e44dc96784cd5f0bd0ac96d8861
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-16 22:30:47 +0200

    ADD_STYLE : to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,33 @@
-import type { Metadata } from 'next';
+'use client';
+// import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { useState } from 'react';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'Mohamed Adel Portfolio',
-  description:
-    'portfolio for Mohamed Adel, frontend developer using React.js and Next.js',
-};
+// export const metadata: Metadata = {
+//   title: 'Mohamed Adel Portfolio',
+//   description:
+//     'portfolio for Mohamed Adel, frontend developer using React.js and Next.js',
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [activeDark, setActiveDark] = useState(true);
+
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={activeDark ? 'dark' : 'light'}>
+      <body
+        className={`${inter.className} bg-light-background dark:bg-dark-background text-light-secondaryForeground dark:text-dark-foreground`}
+      >
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main>hello there</main>;
+  return <main></main>;
 }
