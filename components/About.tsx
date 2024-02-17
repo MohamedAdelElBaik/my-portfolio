7cae90432b5f6e6209d7994ec2bc4c787a3bdf86
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-17 22:04:53 +0200

    CREATE : about component (not finish)

---
+++ components/About.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function About() {
+  return (
+    <section className="min-h-screen flex flex-col gap-8">
+      <h1 className="text-light-primary-r dark:text-dark-primary-r text-4xl font-semibold">
+        About Me
+      </h1>
+      <p className="leading-6">
+        I'm Mohamed Adel, a computer and control engineering graduate from Suez
+        Canal University.
+        <br />
+        <br />
+        I specialize in frontend development using React.js, with expertise in
+        responsive design, state management, React hooks, and RESTful API
+        integration.
+        <br />
+        <br />
+        Passionate about creating seamless user experiences, I blend my
+        engineering background with coding skills to contribute to innovative
+        projects.
+        <br />
+        <br />
+        I enjoy staying updated on the latest industry trends and technologies.
+        <br />
+        <br />
+        Whether it's exploring new frameworks or refining my coding techniques,
+        I am always eager to expand my knowledge and stay at the forefront of
+        the ever-evolving tech landscape.
+        <br />
+        <br />
+        Let's connect and discuss how I can bring value to your next endeavor!
+      </p>
+    </section>
+  );
+}
