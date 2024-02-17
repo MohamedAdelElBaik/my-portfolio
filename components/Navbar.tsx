241de48dcaa5cfe74ea6baa7d2b18b4a23941438
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-17 13:02:36 +0200

    CREATE : navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,82 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
+import style from './Navbar.module.css';
 
-export default function Navbar() {
+export default function Navbar({ activeDark, setActiveDark }) {
+  const [openNav, setOpenNav] = useState(false);
+
   return (
-    <nav className="flex justify-between">
-      <h1>Mohamed Adel</h1>
+    <nav className="flex justify-between items-center bg-amb2er-800 p-3 w-11/12 mx-auto">
+      <h1 className="text-2xl font-bold text-light-primary-r dark:text-dark-primary-r">
+        Mohamed Adel
+      </h1>
 
-      <div className="flex gap-6">
-        <ul className="flex gap-2">
-          <li>Home</li>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Skills</li>
-        </ul>
-        <button className="bg-dark-primary">
-          <Image
-            alt="dark mode toggle"
-            src="./dark.svg"
-            width="40"
-            height="40"
-          />
-        </button>
+      <div>
+        <NavIconToggle openNav={openNav} setOpenNav={setOpenNav} />
+
+        <div
+          className={`${style.navContent} ${
+            openNav ? 'flex' : 'hidden'
+          }  bg-dark-primary-3l md:bg-transparent`}
+        >
+          <ul className="flex flex-col md:static md:flex-row gap-5 font-semibold">
+            <li className="cursor-pointer hover:text-dark-secondary-l">Home</li>
+            <li className="cursor-pointer hover:text-dark-secondary-l">
+              About
+            </li>
+            <li className="cursor-pointer hover:text-dark-secondary-l">
+              Projects
+            </li>
+            <li className="cursor-pointer hover:text-dark-secondary-l">
+              Skills
+            </li>
+          </ul>
+          <div className="flex justify-between gap-8">
+            <button className="py-1 px-2 border border-spacing-1 border-light-primary-r dark:border-dark-primary-r  text-light-primary-r dark:text-dark-primary-r hover:bg-light-primary-3l hover:dark:bg-dark-primary-3l rounded-md">
+              Resume
+            </button>
+            <button
+              className="bg-dark-primary-l dark:bg-dark-secondary-l text-red-50 p-1 rounded-sm"
+              onClick={() => setActiveDark((value) => !value)}
+            >
+              <Image
+                alt="dark mode toggle"
+                src={activeDark ? './dark.svg' : './light.svg'}
+                width="30"
+                height="30"
+              />
+            </button>
+          </div>
+        </div>
       </div>
     </nav>
+  );
+}
+
+function NavIconToggle({ openNav, setOpenNav }) {
+  return (
+    <>
+      {
+        <button
+          onClick={() => setOpenNav((val) => !val)}
+          className="md:hidden flex flex-col w-7 h-5 gap-1 justify-between items-center transition relative cursor-pointer"
+        >
+          {openNav ? (
+            <>
+              <span className="bg-dark-primary-r w-full h-1 rounded-sm -rotate-45 absolute top-1/2 duration-100"></span>
+              <span className="bg-dark-primary-r w-full h-1 rounded-sm rotate-45 absolute top-1/2 duration-100"></span>
+            </>
+          ) : (
+            <>
+              <span className="bg-dark-primary-r w-full h-1 rounded-sm duration-100"></span>
+              <span className="bg-dark-primary-r w-full h-1 rounded-sm duration-100"></span>
+              <span className="bg-dark-primary-r w-full h-1 rounded-sm duration-100"></span>
+            </>
+          )}
+        </button>
+      }
+    </>
   );
 }
