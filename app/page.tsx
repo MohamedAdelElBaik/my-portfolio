f499eee3400ebd9db1c65f439b6e1f259289c8c5
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-17 22:05:37 +0200

    STYLE : add width to all div in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import About from '@/components/About';
 import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
     <main>
-      <Hero />
+      <div className="w-9/12 mx-auto">
+        <Hero />
+        <About />
+      </div>
     </main>
   );
 }
