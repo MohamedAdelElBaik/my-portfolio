544f0126ffa3b45743a3d1a7a0f48baa746073d3
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-17 14:20:59 +0200

    CREATE : hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function Hero() {
+  return (
+    <section className="min-h-screen flex flex-col justify-center w-9/12 mx-auto gap-4">
+      <span className="text-light-primary-r darK:text-dark-primary-r text-sm md:text-base">
+        Hi, my name is
+      </span>
+      <h1 className="text-light-primary-r darK:text-dark-primary-r text-5xl md:text-7xl font-bold">
+        Mohamed Adel
+      </h1>
+      <h2 className="text-2xl md:text-5xl font-semibold">
+        Frontend developer (React.js & Next.js)
+      </h2>
+      <p className="text-base md:text-xl">
+        I'm a passionate Front-End web developer having an experience of web
+        applications with React.js, Next.js, TypeScript and TailwindCSS.
+      </p>
+      <span className="text-sm md:text-base text-dark-primary-l">
+        Currently available for a job & freelance
+      </span>
+    </section>
+  );
+}
