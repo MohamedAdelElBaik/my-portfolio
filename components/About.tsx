88d007aa876601ed571ca6f05bd40076fe9b33ea
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-19 00:55:12 +0200

    FIX_RESPONSIVE : about section

--- components/About.tsx
+++ components/About.tsx
@@ -7,36 +7,31 @@ export default function About() {
       id="about"
       className="min-h-screen flex flex-col justify-center gap-8"
     >
-      <h1 className="text-light-primary-r dark:text-dark-primary-r text-4xl font-semibold">
+      <h1 className="text-light-primary-r dark:text-dark-primary-r text-2xl md:text-4xl font-semibold">
         About Me
       </h1>
-      <p className="leading-6">
+      <div className="leading-6 text-sm md:text-xl">
         I'm Mohamed Adel, a computer and control engineering graduate from Suez
         Canal University.
-        <br />
-        <br />
+        <hr className="border-none h-2 md:h-5" />
         I specialize in frontend development using React.js, with expertise in
         responsive design, state management, React hooks, and RESTful API
         integration.
-        <br />
-        <br />
+        <hr className="border-none h-2 md:h-5" />
         Passionate about creating seamless user experiences, I blend my
         engineering background with coding skills to contribute to innovative
         projects.
-        <br />
-        <br />
+        <hr className="border-none h-2 md:h-5" />
         I enjoy staying updated on the latest industry trends and technologies.
-        <br />
-        <br />
+        <hr className="border-none h-2 md:h-5" />
         Whether it's exploring new frameworks or refining my coding techniques,
         I am always eager to expand my knowledge and stay at the forefront of
         the ever-evolving tech landscape.
-        <br />
-        <br />
+        <hr className="border-none h-2 md:h-5" />
         Let's connect and discuss how I can bring value to your next endeavor!
-      </p>
+      </div>
 
-      <div className="mt-10 w-full border py-6 px-4 border-spacing-1 border-dark-primary-3l rounded-md shadow-2xl shadow-dark-primary-3l">
+      <div className="mt-10 w-full border py-3 px-1 md:py-6 md:px-4 border-spacing-1 border-dark-primary-3l rounded-md shadow-2xl shadow-dark-primary-3l">
         <GitHubCalendar
           username="mohamedadelelbaik"
           blockSize={13}
