545756b97a98e350b60670a8ce10d5cab2079d8e
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-19 01:29:42 +0200

    ADD : social links to contactLink component

--- components/ContactLinks.tsx
+++ components/ContactLinks.tsx
@@ -6,16 +6,28 @@ import TwitterIcon from './svg/TwitterIcon';
 export default function ContactLinks() {
   return (
     <>
-      <a href="" className="hover:scale-110 transition-transform duration-200">
+      <a
+        href="https://github.com/MohamedAdelElBaik"
+        className="hover:scale-110 transition-transform duration-200"
+      >
         <GitHubIcon />
       </a>
-      <a href="" className="hover:scale-110 transition-transform duration-200">
+      <a
+        href="https://www.linkedin.com/in/mohamedadelelbaik/"
+        className="hover:scale-110 transition-transform duration-200"
+      >
         <LinkIcon />
       </a>
-      <a href="" className="hover:scale-110 transition-transform duration-200">
+      <a
+        href="mailto:mohamedadelelbeak@gmail.com"
+        className="hover:scale-110 transition-transform duration-200"
+      >
         <MailIcon />
       </a>
-      <a href="" className="hover:scale-110 transition-transform duration-200">
+      <a
+        href="https://twitter.com/M_ADEL_SALEH"
+        className="hover:scale-110 transition-transform duration-200"
+      >
         <TwitterIcon />
       </a>
     </>
