ef1c48a534e61b34915345b7772ccd9daa4ef644
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-18 23:14:04 +0200

    ADD : projects component to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Skills from '@/components/Skills';
-import Projects from '@/components/Projects';
+import Projects from '@/components/projects/Projects';
 
 export default function Home() {
   return (
