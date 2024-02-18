87f44a4099b1b299c2a4b8803b624aee67cc7d39
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-19 00:53:09 +0200

    FIX_RESPONSIVE : hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,25 +5,27 @@ export default function Hero() {
   return (
     <section
       id="home"
-      className="min-h-screen flex flex-col justify-center gap-4"
+      className="min-h-screen pt-32 flex flex-col justify-center md:flex-row gap-8 items-center"
     >
-      <span className="text-light-primary-r darK:text-dark-primary-r text-sm md:text-base">
-        Hi, my name is
-      </span>
-      <h1 className="text-light-primary-r darK:text-dark-primary-r text-5xl md:text-7xl font-bold">
-        Mohamed Adel
-      </h1>
-      <h2 className="text-2xl md:text-5xl font-semibold">
-        Frontend developer (React.js & Next.js)
-      </h2>
-      <p className="text-base md:text-xl">
-        I'm a passionate Front-End web developer having an experience of web
-        applications with React.js, Next.js, TypeScript and TailwindCSS.
-      </p>
-      <span className="text-sm md:text-base text-dark-primary-l">
-        Currently available for a job & freelance
-      </span>
-      <div className="rounded-lg overflow-hidden object-fill w-72">
+      <div className="flex flex-col justify-center gap-2 md:gap-4 md:flex-1">
+        <span className="text-light-primary-r darK:text-dark-primary-r text-sm md:text-base">
+          Hi, my name is
+        </span>
+        <h1 className="text-light-primary-r darK:text-dark-primary-r text-3xl md:text-7xl font-bold">
+          Mohamed Adel
+        </h1>
+        <h2 className="text-xl md:text-5xl font-semibold">
+          Frontend developer (React.js & Next.js)
+        </h2>
+        <p className="text-sm md:text-xl">
+          I'm a passionate Front-End web developer having an experience of web
+          applications with React.js, Next.js, TypeScript and TailwindCSS.
+        </p>
+        <span className="text-xs md:text-base text-dark-primary-r">
+          Currently available for a job & freelance
+        </span>
+      </div>
+      <div className="rounded-lg overflow-hidden object-fill w-48 md:w-56 lg:w-72 md:flex-grow-0">
         <Image src="/myImage.jpg" alt="mohamed" width={300} height={300} />
       </div>
     </section>
